b640eddbc6e41b44e9d6cd8e268248b377179519
Johan Estrada <johan.manuel.estrada.plaza@gmail.com>
2026-05-10 02:30:07 -0400

    Infraestructura AWS con Terraform

---
+++ infra/grupos_seguridad.tf
@@ -0,0 +1,104 @@
+# =========================
+# SECURITY GROUP FRONTEND
+# =========================
+
+resource "aws_security_group" "frontend_sg" {
+  name        = "${var.project_name}-frontend-sg"
+  description = "Security Group Frontend"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP publico"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH administracion"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-frontend-sg"
+  }
+}
+
+# =========================
+# SECURITY GROUP BACKEND
+# =========================
+
+resource "aws_security_group" "backend_sg" {
+  name        = "${var.project_name}-backend-sg"
+  description = "Security Group Backend"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "API desde frontend"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.frontend_sg.id]
+  }
+
+  ingress {
+    description = "SSH administracion"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-backend-sg"
+  }
+}
+
+# =========================
+# SECURITY GROUP MYSQL
+# =========================
+
+resource "aws_security_group" "mysql_sg" {
+  name        = "${var.project_name}-mysql-sg"
+  description = "Security Group MySQL"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "MySQL desde backend"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.backend_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-mysql-sg"
+  }
+}
